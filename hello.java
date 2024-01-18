c5df8f7fcb4a55e98aa51c6d65df0f7724758d45
kataria5060 <katariabhishek5060@gmail.com>
2024-01-18 11:14:19 +0530

    uyvu

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String [] args){
-        System.out.println("Kataria is Great but Rohit is LODU");
-        System.out.println("Kataria is Great but Rohit is LODU");
+        System.out.println("sahil is Great but abhishek is LODU");
+        System.out.println("sahil is Great but abhishek is LODU");
     }
 
 }
